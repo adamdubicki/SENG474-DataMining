b62ec67d06d146a3c49a0e32f94872e5d2958590
adubicki <addubicki@hotmail.com>
2017-03-18 14:41:12 -0700

    TestingSomeNewStuff

--- data_processor.py
+++ data_processor.py
@@ -53,7 +53,7 @@ def transform_data(line):
 		first_commit = datetime.strptime(line[3], date_format)
 		build_at = datetime.strptime(line[18], date_format)
 		delta = build_at - first_commit
-		if(delta.days < 0):
+		if (delta.days < 0):
 			transformation.append(0)
 		else:
 			transformation.append(delta.days)
@@ -72,7 +72,7 @@ def transform_data(line):
 
 
 def get_data():
-	f = open("data.csv")
+	f = open("data_synth.csv")
 	f2 = open("output.csv", 'w')
 	line = f.readline()
 	count = 0
@@ -113,7 +113,7 @@ def normalize_data():
 				normalization_data[attribute][0] = float(line[attribute])
 			if (float(line[attribute]) < normalization_data[attribute][1]):
 				normalization_data[attribute][1] = float(line[attribute])
-	print ("NORMALIZED ATTRIBUTE DATA",normalization_data)
+	print ("NORMALIZED ATTRIBUTE DATA", normalization_data)
 	f = open("output.csv")
 	f0 = open("d0.csv", 'w')
 	f1 = open("d1.csv", 'w')
@@ -172,10 +172,134 @@ def create_files():
 	open("output.csv", 'a').close()
 
 
+def isfloat(value):
+	try:
+		float(value)
+		return True
+	except ValueError:
+		return False
+
+
+def get_data_with_nulls():
+	f = open("data_synth.csv")
+	f2 = open("output.csv", 'w')
+	line = f.readline()
+	print(line)
+	count = 0
+	startTime = time.time()
+
+	normalized = range(1, 7)
+	normalized_attributes = {}
+	for i in normalized:
+		normalized_attributes[i] = {'max': 0.0, 'min': 99999, 'sum': 0.0, 'not_null': 0.0}
+	print(": : : EXTRACTING DATA : : :")
+
+	for line in f:
+		line = line.strip('\r\n').split(',')
+		count += 1
+		for attribute in normalized:
+			if (line[attribute]) == 'NA' or line[attribute] == '':
+				continue
+			else:
+				if (attribute not in [5, 6]):
+					if float(line[attribute]) > normalized_attributes[attribute]['max']:
+						normalized_attributes[attribute]['max'] = float(line[attribute])
+					if float(line[attribute]) < normalized_attributes[attribute]['min']:
+						normalized_attributes[attribute]['min'] = float(line[attribute])
+					normalized_attributes[attribute]['sum'] += float(line[attribute])
+					normalized_attributes[attribute]['not_null'] += 1
+				if (attribute == 5):
+					if (line[attribute] == 'TRUE'):
+						normalized_attributes[attribute]['sum'] += 1
+					normalized_attributes[attribute]['not_null'] += 1
+				if (attribute == 6):
+					if (line[attribute] == 'passed'):
+						normalized_attributes[attribute]['sum'] += 1
+					normalized_attributes[attribute]['not_null'] += 1
+
+	f = open("data_synth.csv")
+	f0 = open("d0.csv", 'w')
+	f1 = open("d1.csv", 'w')
+	f2 = open("d2.csv", 'w')
+	f3 = open("d3.csv", 'w')
+	f4 = open("d4.csv", 'w')
+	f5 = open("d5.csv", 'w')
+	f6 = open("d6.csv", 'w')
+	f7 = open("d7.csv", 'w')
+	f8 = open("d8.csv", 'w')
+	f9 = open("d9.csv", 'w')
+	line = f.readline()
+	line_count = 0
+	line_made = {}
+	for line in f:
+		new_line = []
+		line = line.strip('\r\n').split(',')
+		if (line[0] == 'ruby'):
+			new_line.append(1)
+			new_line.append(0)
+			new_line.append(0)
+		elif (line[0] == 'java'):
+			new_line.append(0)
+			new_line.append(1)
+			new_line.append(0)
+		else:
+			new_line.append(0)
+			new_line.append(0)
+			new_line.append(1)
+		for i in range(1, 5):
+			new_val = line[i]
+			avg = float(normalized_attributes[i]['sum'] / normalized_attributes[i]['not_null'])
+			data_range = float(normalized_attributes[i]['max'] - normalized_attributes[i]['min'])
+			if (new_val == 'NA' or new_val == '' or not isfloat(new_val)):
+				new_line.append(0.0)
+			else:
+				new_line.append((float(new_val) - avg) / float(data_range))
+		if (line[5] == 'TRUE'):
+			new_line.append(1.0)
+		else:
+			new_line.append(0)
+		if (line[6] == "passed"):
+			new_line.append(1.0)
+		else:
+			new_line.append(-1.0)
+
+		if(tuple(new_line) in line_made):
+			pass
+		else:
+			line_made[tuple(new_line)] = 1
+			if (line_count % 10 == 0):
+				f0.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 1):
+				f1.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 2):
+				f2.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 3):
+				f3.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 4):
+				f4.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 5):
+				f5.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 6):
+				f6.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 7):
+				f7.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 8):
+				f8.write(str(new_line).strip('[,]') + "\n")
+			elif (line_count % 10 == 9):
+				f9.write(str(new_line).strip('[,]') + "\n")
+			line_count += 1
+			print line_count
+	print (normalized_attributes)
+	print(": : : FINISHED NORMALIZING : : :")
+	print("TIME: ", time.time() - startTime)
+
+
+# Grab all of the attributes we want
 def main():
 	# create_files()
-	get_data()
-	normalize_data()
+	# get_data()
+	# normalize_data()
+	get_data_with_nulls()
 
 
 main()
--- log_reg.py
+++ log_reg.py
@@ -1,10 +1,11 @@
 import numpy as np
 import math as m
 import matplotlib.pyplot as plt
+import time
 
 
 def get_sigmoid_val(y, X, w):
-	return float(1.0 / ( 1.0 + np.power(m.exp(1), -1.0 * y * np.dot(X, w.T))))
+	return float(1.0 / (1.0 + np.power(m.exp(1),  -1 * np.dot(X, w.T))))
 
 
 def get_accuracy(w, x_data, y_data, thresh):
@@ -20,6 +21,7 @@ def get_accuracy(w, x_data, y_data, thresh):
 
 	for i in range(0, y_data.shape[0]):
 		sig = get_sigmoid_val(y_data[i], x_data[i], w)
+		# print(sig, y_data[i])
 		if sig > thresh:
 			good_builds = np.append(good_builds, [sig])
 			guess = 1.0
@@ -50,8 +52,11 @@ def get_accuracy(w, x_data, y_data, thresh):
 	print ("True Negatives", tn)
 	print ("False Positives", fp)
 	print ("False Negatives", fn)
-	print ("Accuracy", float((tp+tn) / (y_data.shape[0])))
-	return float((tp+tn) / (y_data.shape[0]))
+	print ("Accuracy", float((tp + tn) / (y_data.shape[0])))
+	# plt.plot(good_builds, good_builds, 'r')
+	# plt.plot(failing_builds, failing_builds, 'b')
+	# plt.show()
+	return float((tp + tn) / (y_data.shape[0]))
 
 
 def gradient_descent(y, X, w):
@@ -65,22 +70,37 @@ def error(y, X, w):
 	return (1.0 / y.shape[0]) * np.sum(grad, axis=0)
 
 
-def main():
-	training_data = ['d1.csv', 'd2.csv', 'd3.csv', 'd4.csv', 'd5.csv', 'd6.csv','d7.csv','d8.csv', 'd9.csv']
-	testing_data = 'd0.csv'
-	kappa = 2.0
-	w_accuracy = np.array([])
+def get_roc(test_data, w):
+	data = np.genfromtxt(test_data, delimiter=',')
+	num_attributes = (data[0].shape[0] - 1)
+	x_data = data[:, 0:num_attributes]
+	y_data = data[:, num_attributes]
+	dummy_col = np.ones((x_data.shape[0], 1))
+	x_data = np.concatenate((dummy_col, x_data,), axis=1)
 
-	w = np.zeros(20)
-	# w = np.zeros(3)
+	good_builds = np.array([])
+	failing_builds = np.array([])
+	for i in range(0, y_data.shape[0]):
+		sig = get_sigmoid_val(y_data[i], x_data[i], w)
+		if (y_data[i] == 1.0):
+			good_builds = np.append(good_builds, [sig])
+		else:
+			failing_builds = np.append(failing_builds, [sig])
+
+	good_builds.sort()
+	failing_builds.sort()
+	plt.plot(np.ones(good_builds.shape[0]), good_builds, 'r')
+	plt.plot(-1 * np.ones(failing_builds.shape[0]), failing_builds, 'b')
+	plt.show()
+	print(good_builds)
+	print(failing_builds)
+
+
+def train_classifier(training_data):
+	kappa = 2.0
+	w = np.zeros(9)
+
 	for i in range(0, 10):
-		data = np.genfromtxt(testing_data, delimiter=',')
-		num_attributes = (data[0].shape[0] - 1)
-		x_data = data[:, 0:num_attributes]
-		y_data = data[:, num_attributes]
-		dummy_col = np.ones((x_data.shape[0], 1))
-		x_data = np.concatenate((dummy_col, x_data,), axis=1)
-		w_accuracy = np.append(w_accuracy, [get_accuracy(w, x_data, y_data, 0.5)])
 		for t in training_data:
 			data = np.genfromtxt(t, delimiter=',')
 			num_attributes = (data[0].shape[0] - 1)
@@ -89,10 +109,16 @@ def main():
 			dummy_col = np.ones((x_data.shape[0], 1))
 			x_data = np.concatenate((dummy_col, x_data,), axis=1)
 			w = w + (kappa * (gradient_descent(y_data, x_data, w)))
-	plt.plot(w_accuracy)
-	plt.ylabel('Accuracy')
-	plt.show()
-	print(w)
+	print w
+	return w
 
+
+def main():
+	# Train w on a training set
+	# w = train_classifier(['d0.csv', 'd1.csv', 'd2.csv', 'd3.csv', 'd4.csv', 'd5.csv', 'd6.csv', 'd7.csv', 'd8.csv'])
+	# Or just use a w to get roc quicker
+	w = np.array(
+		[[0.45339011, 0.12258675, 0.28721841, 0.04358495, -0.0526, -0.13355728, -0.00250463, -0.01697852, 0.33722971]])
+	get_roc('d0.csv', w)
 
 main()
